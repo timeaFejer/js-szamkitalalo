4d5f554121f4d895af7a81c1303f42d1b0bb2bd7
timeaFejer <timea.fejer88@gmail.com>
2025-04-23 21:06:52 +0200

    reset game

--- szamkitalalo.js
+++ szamkitalalo.js
@@ -1,7 +1,25 @@
+let guessListContainer = document.querySelector('js-guess-container');  //a változónév elé írhatunk $ jelet, ezzel is jelölve, hogy dokumentum csomópont 
+let currentGuessField = document.querySelector('js-guess-field');
+let newGameButton = document.querySelector('js-new-game-button');
+
+let target;
+
 function generateTarget() {
     return Math.floor(Math.random() * 100 ) + 1;
 }
 
+function resetGame() {
+    // korábbi tippek törlése
+    guessListContainer.innerHTML = "";
+    // input mező tartalmának törlése
+    currentGuessField.value = "";
+}
 
+function startGame() {
+    resetGame();
+    target = generateTarget();
+}
 
-console.log(generateTarget());
+startGame();
+newGameButton.addEventListener('click', startGame)
+
